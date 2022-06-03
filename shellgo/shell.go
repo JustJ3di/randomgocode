ce3ef476edd991cbb024fcd7d1962db3fd48164d
Emanuele <emanuele.dajello@gmail.com>
2022-06-03 15:05:30 +0200

    new

---
+++ shellgo/shell.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func exec_from_input(input string) error {
+
+	input = strings.TrimSuffix(input, "\n")
+
+	args := strings.Split(input, " ")
+
+	if args[0] == "exit" {
+		os.Exit(0)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	var vect []int
+	vect = make([]int, 10)
+
+	for i := 0; i < 6; i++ {
+		vect[i] = i + 1
+	}
+
+	fmt.Println(vect)
+	fmt.Println(len(vect))
+	fmt.Println(cap(vect))
+
+	for {
+		fmt.Print("$$> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		error := exec_from_input(input)
+		if error != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+
+}
